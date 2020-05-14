1df91e180cf2f25239e03635711ff07fedd8e1e3
Luis Angel Larios Castillo <luis44larios.99@gmail.com>
2020-05-14 14:12:55 -0500

    Version final portafolio.html

--- core/views.py
+++ core/views.py
@@ -9,8 +9,5 @@ def home(request):
 def about(request):
      return render(request,"core/about.html")
 
-def portfolio(request):
-    return render(request,"core/portfolio.html")
-
 def contact(request):
     return render(request,"core/contact.html")
--- portfolio/models.py
+++ portfolio/models.py
@@ -5,8 +5,10 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titulo")
     description = models.TextField(verbose_name="Descripcíon")
     image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    link = models.URLField(verbose_name="Dirección Web", null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+    
 
     class Meta:
         verbose_name = "proyecto"
